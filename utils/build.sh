41e62066abc087938a672038b6704231a4765b0c
tamat <contact@tamats.com>
2020-08-02 22:22:57 +0200

    fixes

--- utils/build.sh
+++ utils/build.sh
@@ -3,5 +3,5 @@ cp ../../collada/src/* ../external
 cp ../../litegl/build/* ../external
 cp ../../litegraph/build/* ../external
 cp ../../canvas2DtoWebGL/src/Canvas2DtoWebGL.js ../external
-python builder.py deploy_files.txt -o ../build/litescene.min.js -o2 ../build/litescene.js 
-chmod a+rw ../build/* 
+python builder.py deploy_files.txt -o ../build/onecore.min.js -o2 ../build/onecore.js
+chmod a+rw ../build/*
